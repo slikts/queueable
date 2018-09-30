55e9551db5653e7e1fab2a857e3b7626e1515891
slikts <dabas@untu.ms>
2018-09-30 20:28:05 +0300

    combinators

--- src/iteration.ts
+++ src/iteration.ts
@@ -1,5 +1,3 @@
-import AsyncQueue from './AsyncQueue'
-
 type AI<A> = AsyncIterable<A>
 
 export const map = async function*<A, B>(as: AI<A>, f: (a: A) => B): AsyncIterableIterator<B> {
@@ -9,8 +7,8 @@ export const map = async function*<A, B>(as: AI<A>, f: (a: A) => B): AsyncIterab
 }
 
 export const filter = async function*<A>(
-  as: AI<A>,
   p: (a: A) => boolean,
+  as: AI<A>,
 ): AsyncIterableIterator<A> {
   for await (const a of as) {
     if (p(a)) {
@@ -19,15 +17,10 @@ export const filter = async function*<A>(
   }
 }
 
-export const forward = async <A>(source: AI<A>, destination: AsyncQueue<A>): Promise<void> => {
-  await map(source, (a: A) => {
-    destination.push(a)
-  })
-}
-
-export const merge = <A>(a1: AI<A>, a2: AI<A>): AI<A> => {
-  const queue: AsyncQueue<A> = new AsyncQueue()
-  forward(a1, queue)
-  forward(a2, queue)
-  return queue
+export const reduce = async <A, B>(f: (a: A, b: B) => A, init: A, bs: AI<B>): Promise<A> => {
+  let a: A = init
+  for await (const b of bs) {
+    a = f(a, b)
+  }
+  return a
 }
---
+++ test/iteration.test.ts
@@ -0,0 +1,31 @@
+import { map, filter, reduce } from '../src/iteration'
+
+const numbers = async function*() {
+  for (let i = 1; i < 5; i += 1) {
+    yield i
+  }
+}
+
+it('maps', async () => {
+  const ns = numbers()
+  const result = map(ns, String)
+  expect(await Promise.all([result.next(), result.next()])).toEqual([
+    { done: false, value: '1' },
+    { done: false, value: '2' },
+  ])
+})
+
+it('filters', async () => {
+  const ns = numbers()
+  const result = filter(i => i > 1, ns)
+  expect(await Promise.all([result.next(), result.next()])).toEqual([
+    { done: false, value: 2 },
+    { done: false, value: 3 },
+  ])
+})
+
+it('reduces', async () => {
+  const ns = numbers()
+  const result = reduce((a, b) => a + b, 0, ns)
+  expect(await result).toBe(10)
+})
