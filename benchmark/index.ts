5e644f6c2795509e70fb3ad67a5fa453898e9cde
slikts <dabas@untu.ms>
2018-10-09 01:13:17 +0300

    ts bench

---
+++ benchmark/index.ts
@@ -0,0 +1,26 @@
+import Balancer from "../src/Balancer";
+
+const iterations = 1e5;
+const now = () => (process.hrtime as any).bigint();
+
+const test = (q: any, f: any, n: any, label: any) => {
+  const t0 = now();
+  for (let i = 0; i < n; i += 1) {
+    f(i, q);
+  }
+  const delta = now() - t0;
+  console.log(delta, label);
+  return delta;
+};
+
+const t1 = test(new Balancer(), (n: any, q: any) => {
+  q.push(n);
+  if (n % 10 === 0) q.next();
+}, iterations, `Balancer`);
+
+const t2 = test([], (n: any, q: any) => {
+  q.push(n);
+  if (n % 10 === 0) q.shift();
+}, iterations, `Array.shift()`);
+
+console.log(`${Number(t2) / Number(t1)}x`);
