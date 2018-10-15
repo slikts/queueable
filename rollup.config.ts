7ff3fcb119e83890548ed00f12974aa5cd23e801
slikts <dabas@untu.ms>
2018-10-15 16:06:04 +0300

    lowercase bundle name

--- rollup.config.ts
+++ rollup.config.ts
@@ -3,18 +3,15 @@
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import sourceMaps from 'rollup-plugin-sourcemaps'
-import camelCase from 'lodash.camelcase'
 import typescript from 'rollup-plugin-typescript2'
 import json from 'rollup-plugin-json'
-
-const pkg = require('./package.json')
 
 const libraryName = 'Queueable'
 
 export default {
   input: `src/index.ts`,
   output: [
-    { file: `dist/${libraryName}.umd.js`, name: camelCase(libraryName), format: 'umd', sourcemap: true },
+    { file: `dist/${libraryName.toLowerCase()}.umd.js`, name: libraryName, format: 'umd', sourcemap: true },
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
   external: [],
