375a71161268b03d12e035d112eb6e801e1c0376
slikts <dabas@untu.ms>
2018-11-23 14:51:22 +0200

    fix: export LastResult

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import Multicast from './adapters/Multicast';
 import Channel from './adapters/Channel';
+import LastResult from './adapters/LastResult';
 import Deferred from './Deferred';
 
-export { Multicast, Channel, Deferred };
+export { Multicast, Channel, Deferred, LastResult };
 export * from './from';
