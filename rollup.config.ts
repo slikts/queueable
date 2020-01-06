000aaafdf839c0e27bbd1e2b3efd9b5b879b9301
slikts <dabas@untu.ms>
2020-01-06 13:22:31 +0000

    build: replace TS plugin

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,17 +1,22 @@
 /* tslint:disable */
 
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
-import sourceMaps from 'rollup-plugin-sourcemaps'
-import typescript from 'rollup-plugin-typescript2'
-import json from 'rollup-plugin-json'
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import sourceMaps from 'rollup-plugin-sourcemaps';
+import ts from '@wessberg/rollup-plugin-ts';
+import json from 'rollup-plugin-json';
 
-const libraryName = 'Queueable'
+const libraryName = 'Queueable';
 
 export default {
   input: `src/index.ts`,
   output: [
-    { file: `dist/${libraryName.toLowerCase()}.umd.js`, name: libraryName, format: 'umd', sourcemap: true },
+    {
+      file: `dist/${libraryName.toLowerCase()}.umd.js`,
+      name: libraryName,
+      format: 'umd',
+      sourcemap: true,
+    },
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
   external: [],
@@ -22,7 +27,7 @@ export default {
     // Allow json resolution
     json(),
     // Compile TypeScript files
-    typescript({ useTsconfigDeclarationDir: true }),
+    ts(),
     // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
     commonjs(),
     // Allow node_modules resolution, so you can use 'external' to control
@@ -33,4 +38,4 @@ export default {
     // Resolve source maps to the original source
     sourceMaps(),
   ],
-}
+};
